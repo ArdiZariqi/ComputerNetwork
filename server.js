1eee737dcf3109a984c5309da38831d61ab66fbb
ArdiZariqi <zariqiardi@gmail.com>
2023-11-28 20:13:15 +0100

    some changes in the server code

--- server.js
+++ server.js
@@ -1,27 +1,72 @@
-var PORT = 33333;
-var HOST = '0.0.0.0';
+const UDP = require('dgram');
 const fs = require('fs');
-var path = 'example.txt';
-errorcode = "Nuk keni qasje!"
 
-const { info } = require('console');
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
+const server = UDP.createSocket('udp4');
+const serverPort = 2222;
+const serverAddress = '0.0.0.0';
+
+const allowedFullAccessClient = { clientIpAddress: '192.168.0.109' };
+
+const connectedClients = [];
 
 server.on('listening', function () {
     var address = server.address();
-    console.log(`Serveri udp eshte aktiv ne adresen: ${address.address}:${address.port}`);
+    console.log(`Serveri udp eshte aktiv ne adresen:  ${address.address}:${address.port}`);
 });
 
-server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port + ' - ' + message);
-    const reply = 'Mesazhi ka arritur me sukses ne server!';
-    server.send(reply, 0, reply.length, remote.port, remote.address, function (err, bytes) {
-        if (err) throw err;
-        console.log(
-            `Pergjigja e serverit u dergua tek klienti ${remote.address}:${remote.port}`
-        );
+server.on('message', (message, remote) => {
+    const request = message.toString().trim().split(' ');
+    const command = request[0].toUpperCase();
+
+    handleClientConnect(remote);
+
+    // Check if the client is allowed for full access
+    if (remote.address == allowedFullAccessClient.clientIpAddress) {
+        switch (command) {
+            case 'READ':
+                handleReadFile(request.slice(1).join(' '), remote);
+                break;
+            case 'WRITE':
+                handleWriteFile(request.slice(1).join(' '), remote);
+                break;
+            case 'EXECUTE':
+                handleExecuteCommand(request.slice(1).join(' '), remote);
+                break;
+            case 'MESSAGE':
+                handleMessage(request.slice(1).join(' '), remote);
+                break;
+            default:
+                console.log('Invalid command from client');
+        }
+    } else {
+        if (command === 'READ') {
+            handleReadFile(request.slice(1).join(' '), remote);
+        } else {
+            const response = Buffer.from(`You don't have permissions to ${command}`);
+            server.send(response, 0, response.length, remote.port, remote.address);
+            console.log(`Unauthorized access attempt from ${remote.address}:${remote.port}`);
+        }
+
+    }
+});
+
+server.bind(serverPort, serverAddress);
+
+process.on('SIGINT', () => {
+    console.log('Server is terminating...');
+    server.close(() => {
+        console.log('Server terminated.');
+        process.exit();
     });
 });
 
-server.bind(PORT, HOST);
+function handleClientConnect(remote) {
+    const existingClient = connectedClients.find(
+        (client) => client.address === remote.address && client.port === remote.port
+    );
+
+    if (!existingClient) {
+        connectedClients.push({ address: remote.address, port: remote.port });
+        console.log(`Client connected: ${remote.address}:${remote.port} `);
+    }
+}
